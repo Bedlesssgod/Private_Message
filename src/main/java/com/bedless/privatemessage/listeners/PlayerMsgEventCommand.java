19f8d23ded6b1b0a8f8c432dd97b6b849d20db67
bedless <bedlesssgod@gmail.com>
2021-12-31 14:49:53 +0100

    MultiVersion Support

--- src/main/java/com/bedless/privatemessage/listeners/PlayerMsgEventCommand.java
+++ src/main/java/com/bedless/privatemessage/listeners/PlayerMsgEventCommand.java
@@ -11,41 +11,43 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 public class PlayerMsgEventCommand implements Listener {
     @EventHandler
     public void onCommandSend(PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().toLowerCase().contentEquals("/msg")) {
-            e.setCancelled(true);
-            e.getPlayer().sendMessage(ChatColor.RED + "Incorrect Usage");
-            e.getPlayer().sendMessage(ChatColor.RED + "Example Usage " + ChatColor.GRAY + "'/msg playername message'");
-        }
-        if (!e.getMessage().toLowerCase().startsWith("/msg"))
-            return;
-        String msgraw = e.getMessage().replace("/msg ", "");
-        String[] msgrawS = msgraw.split(" ");
-        String msgraw3 = msgraw.replace(msgrawS[0] + " ", "");
-        String msgplayerName = msgrawS[0];
-        String message = msgraw.replace(msgplayerName, "");
-        Player msgp = e.getPlayer();
-        Player msgp2 = Bukkit.getPlayerExact(msgplayerName);
-        if (message.length() == 0) {
-            e.setCancelled(true);
-            //p.sendMessage(ChatColor.GRAY + "All messages will be transferred to the Private channel! with " + p2.getName());
-        }
-        if (message.length() >= 1) {
-            e.setCancelled(true);
-            try {
-                Privatemessage.getInstance().getConfig();
-                String msgrecieve = Privatemessage.getInstance().getConfig().getString("playermsgrecieve");
-                String msgsend = Privatemessage.getInstance().getConfig().getString("playermsgsend");
-                ;
-                msgrecieve = msgrecieve.replaceAll("%player%", msgp2.getDisplayName());
-                msgrecieve = msgrecieve.replaceAll("%message%", message);
-                msgsend = msgsend.replaceAll("%player%", msgp2.getDisplayName());
-                msgsend = msgsend.replaceAll("%message%", message);
-                if (message.length() > 3)
-                    msgp2.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', msgrecieve));
-                msgp.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', msgsend));
-            } catch (NullPointerException ex) {
-                msgp.sendMessage(ChatColor.RED + "Couldn't find a player by the name of '" + msgplayerName + "'");
-                ex.printStackTrace();
+        if (e.getPlayer().hasPermission("privmsg.msg")) {
+            if (e.getMessage().toLowerCase().contentEquals("/msg")) {
+                e.setCancelled(true);
+                e.getPlayer().sendMessage(ChatColor.RED + "Incorrect Usage");
+                e.getPlayer().sendMessage(ChatColor.RED + "Example Usage " + ChatColor.GRAY + "'/msg playername message'");
+            }
+            if (!e.getMessage().toLowerCase().startsWith("/msg"))
+                return;
+            String msgraw = e.getMessage().replace("/msg ", "");
+            String[] msgrawS = msgraw.split(" ");
+            String msgraw3 = msgraw.replace(msgrawS[0] + " ", "");
+            String msgplayerName = msgrawS[0];
+            String message = msgraw.replace(msgplayerName, "");
+            Player msgp = e.getPlayer();
+            Player msgp2 = Bukkit.getPlayerExact(msgplayerName);
+            if (message.length() == 0) {
+                e.setCancelled(true);
+                //p.sendMessage(ChatColor.GRAY + "All messages will be transferred to the Private channel! with " + p2.getName());
+            }
+            if (message.length() >= 1) {
+                e.setCancelled(true);
+                try {
+                    Privatemessage.getInstance().getConfig();
+                    String msgrecieve = Privatemessage.getInstance().getConfig().getString("playermsgrecieve");
+                    String msgsend = Privatemessage.getInstance().getConfig().getString("playermsgsend");
+                    ;
+                    msgrecieve = msgrecieve.replaceAll("%player%", msgp2.getDisplayName());
+                    msgrecieve = msgrecieve.replaceAll("%message%", message);
+                    msgsend = msgsend.replaceAll("%player%", msgp2.getDisplayName());
+                    msgsend = msgsend.replaceAll("%message%", message);
+                    if (message.length() > 3)
+                        msgp2.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', msgrecieve));
+                    msgp.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', msgsend));
+                } catch (NullPointerException ex) {
+                    msgp.sendMessage(ChatColor.RED + "Couldn't find a player by the name of '" + msgplayerName + "'");
+                    ex.printStackTrace();
+                }
             }
         }
     }
--- src/main/java/com/bedless/privatemessage/listeners/PlayerTellEventCommand.java
+++ src/main/java/com/bedless/privatemessage/listeners/PlayerTellEventCommand.java
@@ -12,43 +12,45 @@ public class PlayerTellEventCommand implements Listener {
 
     @EventHandler
     public void onCommandSend(PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().toLowerCase().contentEquals("/tell")) {
-            e.setCancelled(true);
-            e.getPlayer().sendMessage(ChatColor.RED + "Incorrect Usage");
-            e.getPlayer().sendMessage(ChatColor.RED + "Example Usage " + ChatColor.GRAY + "'/tell playername message'");
-        }
-        if (!e.getMessage().toLowerCase().startsWith("/tell"))
-            return;
-        String tellraw = e.getMessage().replace("/tell ", "");
-        String[] tellrawS = tellraw.split(" ");
-        String tellraw3 = tellraw.replace(tellrawS[0] + " ", "");
-        String tellplayerName = tellrawS[0];
-        String tellmessage = tellraw.replace(tellplayerName, "");
-        Player tellp = e.getPlayer();
-        Player tellp2 = Bukkit.getPlayerExact(tellplayerName);
-
-        if (tellmessage.length() == 0) {
-            e.setCancelled(true);
-            //p.sendMessage(ChatColor.GRAY + "All messages will be transferred to the Private channel! with " + p2.getName());
-        }
-        if (tellmessage.length() >= 1) {
-            e.setCancelled(true);
-            try {
-                Privatemessage.getInstance().getConfig();
-                String tellrecieve = Privatemessage.getInstance().getConfig().getString("tellrecieve");
-                String tellsend = Privatemessage.getInstance().getConfig().getString("tellsend");
-                tellrecieve = tellrecieve.replaceAll("%player%", tellp2.getDisplayName());
-                tellrecieve = tellrecieve.replaceAll("%message%", tellmessage);
-                tellsend = tellsend.replaceAll("%player%", tellp2.getDisplayName());
-                tellsend = tellsend.replaceAll("%message%", tellmessage);
-                if (tellmessage.length() > 3)
-                    tellp2.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', tellrecieve));
-                tellp.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', tellsend));
-            } catch (NullPointerException ex) {
-                tellp.sendMessage(ChatColor.RED + "Couldn't find a player by the name of '" + tellplayerName + "'");
-                ex.printStackTrace();
+        if (e.getPlayer().hasPermission("privmsg.tell")) {
+            if (e.getMessage().toLowerCase().contentEquals("/tell")) {
+                e.setCancelled(true);
+                e.getPlayer().sendMessage(ChatColor.RED + "Incorrect Usage");
+                e.getPlayer().sendMessage(ChatColor.RED + "Example Usage " + ChatColor.GRAY + "'/tell playername message'");
             }
+            if (!e.getMessage().toLowerCase().startsWith("/tell"))
+                return;
+            String tellraw = e.getMessage().replace("/tell ", "");
+            String[] tellrawS = tellraw.split(" ");
+            String tellraw3 = tellraw.replace(tellrawS[0] + " ", "");
+            String tellplayerName = tellrawS[0];
+            String tellmessage = tellraw.replace(tellplayerName, "");
+            Player tellp = e.getPlayer();
+            Player tellp2 = Bukkit.getPlayerExact(tellplayerName);
 
+            if (tellmessage.length() == 0) {
+                e.setCancelled(true);
+                //p.sendMessage(ChatColor.GRAY + "All messages will be transferred to the Private channel! with " + p2.getName());
+            }
+            if (tellmessage.length() >= 1) {
+                e.setCancelled(true);
+                try {
+                    Privatemessage.getInstance().getConfig();
+                    String tellrecieve = Privatemessage.getInstance().getConfig().getString("tellrecieve");
+                    String tellsend = Privatemessage.getInstance().getConfig().getString("tellsend");
+                    tellrecieve = tellrecieve.replaceAll("%player%", tellp2.getDisplayName());
+                    tellrecieve = tellrecieve.replaceAll("%message%", tellmessage);
+                    tellsend = tellsend.replaceAll("%player%", tellp2.getDisplayName());
+                    tellsend = tellsend.replaceAll("%message%", tellmessage);
+                    if (tellmessage.length() > 3)
+                        tellp2.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', tellrecieve));
+                    tellp.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', tellsend));
+                } catch (NullPointerException ex) {
+                    tellp.sendMessage(ChatColor.RED + "Couldn't find a player by the name of '" + tellplayerName + "'");
+                    ex.printStackTrace();
+                }
+
+            }
         }
     }
 }
